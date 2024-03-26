028b1ca91e8521b3cd95976f4bf934bf92bf03e4
Dilraj Singh <dilraj082@gmail.com>
2024-03-26 19:12:14 +0530

    starting backend for navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -6,32 +6,25 @@ const Navbar = () => {
     
     return (
         <div className="navbar bg-[#166C57] p-8 text-[#B3C1D6]">
+
             <div className="navbar-start pl-44">
                 <a className="btn btn-ghost text-4xl font-extrabold ">
                 <img src={logo} alt="app logo" className='h-10 w-10' />ShopSmart
                 </a>
             </div>
 
-            <div className="navbar-center hidden lg:flex font-black">
-                <ul className="menu menu-horizontal px-1">
-                <li><a>Item 1</a></li>
-                <li>
-                    <details>
-                    <summary>Parent</summary>
-                    <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                    </ul>
-                    </details>
-                </li>
-                <li><a>Item 3</a></li>
-                </ul>
+            <div className="navbar-center gap-2">
+                <div className="form-control lg:flex">
+                    <input type="text" placeholder="Search Product" className="input input-bordered w-80 h-12 text-black bg-white" />
+                </div>
+                <a href="" className="btn btn-ghost text-xl font-black text-[#B3C1D6] bg-black hover:underline">Search</a>
            </div>
 
             <div className="navbar-end hidden lg:flex space-x-4 pr-44">
                 <a className='btn btn-ghost text-xl font-black text-[#B3C1D6] bg-transparent hover:underline'>Signup</a>
                 <a className="btn btn-ghost text-xl font-black text-[#B3C1D6] bg-transparent hover:underline">Login</a>
             </div>
+
         </div>
     )
 }
